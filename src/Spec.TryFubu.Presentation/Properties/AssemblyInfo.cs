4b0ec337090da0bfbc59b0889d4d03a4c4cdc204
walkingdisaster <e.torque.bentley@gmail.com>
2011-01-19 10:28:32 -0500

    fixed typo in spec project name... presntation > presentation

--- src/Spec.TryFubu.Presentation/Properties/AssemblyInfo.cs
+++ src/Spec.TryFubu.Presentation/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Spec.TryFubu.Presntation")]
+[assembly: AssemblyTitle("Spec.TryFubu.Presentation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Spec.TryFubu.Presntation")]
+[assembly: AssemblyProduct("Spec.TryFubu.Presentation")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
